c32dc12eb0333fd3f325819e06ae9899fdf800a3
Nancy <nancymkhize2@gmail.com>
2023-12-11 17:18:10 +0200

    Create the PreviewsLayout component, which serves as a layout for displaying previews of shows. The component incorporates search functionality, sorting options, and individual Preview components for each show.

---
+++ src/layouts/PreviewsLayout.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import Fuse from 'fuse.js';
+import { Preview } from "../components/Preview";
+import { useSort } from "../hooks/useSort";
+import { SortOptions } from "../components/SortOptions";
+import { BiSearch, BiLoader } from "react-icons/bi"; // Bootstrap icons
+
+export default function PreviewsLayout(props) {
+  const { shows } = props;
+  const fuse = new Fuse(shows, {
+    keys: ['title'],
+  });
+  const [searchQuery, setSearchQuery] = useState('');
+  const searchResults = fuse.search(searchQuery);
+  const reducedSearch = searchQuery ? searchResults.map((obj) => obj.item) : shows;
+
+  const [sort, setSort] = useState("none");
+  const sortedShows = useSort(reducedSearch, sort);
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+    setSort('none');
+  };
+
+  if (!sortedShows) {
+    return <BiLoader className="animate-spin text-dark-gray text-6xl py-12" />;
+  }
+
+  const allPreviews = sortedShows.map((show) => {
+    return <Preview key={show.id} showId={show.id} genres={show.genres} />;
+  });
+
+  return (
+    <section className="bg-platinum p-4 flex flex-col gap-2">
+      <div className="flex justify-between">
+        <SortOptions setSort={setSort} />
+        <div className="w-[35%] bg-dark-gray relative text-teal text-[0.8em] text">
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={handleSearch}
+            value={searchQuery}
+            className="indent-1 h-[98%] w-[75%] bg-dark-gray absolute right-1 top-0"
+          />
+          <BiSearch className="absolute left-1 top-2" />
+        </div>
+      </div>
+      {allPreviews}
+    </section>
+  );
+}
