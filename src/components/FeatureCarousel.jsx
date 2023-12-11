90d05b7ed64b6dde29357f3c31f7e4ed1fec9ebc
Nancy <nancymkhize2@gmail.com>
2023-12-11 15:02:31 +0200

    Introduce the FeaturedCarousel component, which dynamically generates a carousel of featured shows based on the data retrieved using the useLoaderData hook. The component uses the FeaturedCard component to represent each featured show within the carousel.

---
+++ src/components/FeatureCarousel.jsx
@@ -0,0 +1,24 @@
+import { Link, useLoaderData } from "react-router-dom"
+import FeaturedCard from "../FeaturedCard";
+import Carousel from "../Carousel";
+
+const FeatureCarousel = () => {
+  const shows = useLoaderData();
+  if(!shows) {
+    return <h1>Loading...</h1>
+  }
+
+  const FeaturedShows = shows.map((show, index) => {
+    return (
+      <FeaturedCard key={index} id={show.id}/>
+    )
+  })
+
+  return (
+    <>
+      <Carousel name='Picked for You' cards={FeaturedShows} />
+    </>
+  )
+}
+
+export default FeatureCarousel
